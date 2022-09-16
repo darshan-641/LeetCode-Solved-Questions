9ca659c072bcc16039cd68ca735c27c32352112b
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-09-16 21:22:50 +0530

    Time: 7 ms (33.17%), Space: 8.3 MB (81.16%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,18 +2,31 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         
-        
-        // priority_queue<int, vector<int>, greater<int>> pq;
-        
-        unordered_map<int,int> mp;
+        priority_queue<int, vector<int>, greater<int>> pq;
         
         for( int i =0; i< nums.size(); i++){
             
-            if( nums[i] != 0){
-                mp[nums[i]]++;
+            pq.push(nums[i]);
+        }
+        
+        int min = 0;
+        int count = 0;
+        
+        while( !pq.empty()){
+            
+            if( pq.top() == 0) pq.pop();
+            else{
+//                 pq top not 0
+                int top = pq.top() - min;
+                pq.pop();
+                if( top != 0){
+                    min += top;
+                    count++;
+                }
+                
             }
         }
         
-        return mp.size();
+        return count;
     }
 };
