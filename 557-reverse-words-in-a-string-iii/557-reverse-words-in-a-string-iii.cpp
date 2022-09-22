ab45962d5578d55ee577321b2b0643c22a51c4c3
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-09-22 10:31:25 +0530

    Time: 143 ms (7.29%), Space: 268.5 MB (7.24%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    string reverse1( string temp){
+    void reverse1( string &temp){
         int i =0;
         int j = temp.length()-1;
         
         while( i <= j){
             swap(temp[i++], temp[j--]);
         }
-        return temp;
+        return ;
     }
     string reverseWords(string s) {
         
@@ -17,14 +17,14 @@ public:
         for( int i =0; i<s.size(); i++){
             
             if( s[i] == ' '){
-                
-                ans = ans + reverse1(temp) + s[i];
+                reverse1(temp);
+                ans = ans + temp+ s[i];
                 temp.clear();
             }
             else temp += s[i];
         }
-        
-        ans += reverse1(temp);
+        reverse1(temp);
+        ans += temp;
         
         return ans;
         
