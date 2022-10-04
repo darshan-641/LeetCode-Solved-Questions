b8868762cadaf637b4b1aef3493c6f8cc7335e02
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-04 16:11:10 +0530

    Time: 778 ms (83.56%), Space: 190.7 MB (46.13%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void addEdge( unordered_map<int, list<int>> &adj, vector<vector<int>>& edges){
+        
+        for( int i =0; i< edges.size(); i++){
+            
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            
+        }
+    }
+    
+    bool bfs( unordered_map<int, list<int>> &adj, unordered_map<int, bool> &visited, int source, int destination ){
+        
+        queue<int> q;
+        q.push(source);
+        // visited[source] = 1;
+        
+        while( !q.empty()){
+            
+            int frontNode = q.front();
+            q.pop();
+            
+            if( frontNode == destination) return true;
+            
+            visited[source] = 1;
+            
+            for( auto i : adj[frontNode]){
+                
+                if( !visited[i]){
+                    q.push(i);
+                    visited[i] = 1;
+                }
+            }
+        }
+        
+        return false;
+    } 
+    
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+       
+        // if( source == destination) return true;
+        
+        unordered_map<int, list<int>> adj;
+        
+        addEdge(adj, edges);
+        
+        unordered_map<int, bool> visited;
+        
+    
+        return bfs( adj, visited, source, destination);
+    }
+};
