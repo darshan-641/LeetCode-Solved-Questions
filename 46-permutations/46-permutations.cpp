5bc1e7a9cb95b83e9c46f599f0b25bc7f47de9cc
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-09 23:06:14 +0530

    Time: 10 ms (21.56%), Space: 8.1 MB (19.30%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void solve( int index, int size, vector<int> &nums, vector<vector<int>> &ans){
+    void solve( int index, int size, vector<int> nums, vector<vector<int>> &ans){
         
         if( index == size){
             ans.push_back(nums);
@@ -11,7 +11,7 @@ public:
         for( int k = index; k<size; k++){
            swap(nums[k],nums[index]);
             solve(index+1,size,nums,ans);
-           swap(nums[k],nums[index]);
+           // swap(nums[k],nums[index]);
         }
     }
     
