b8409a0e06a22fa2b6cbb6b601dad734cd5c2f8e
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-22 12:44:03 +0530

    Time: 6 ms (21.66%), Space: 8 MB (19.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -16,14 +16,33 @@ public:
         
     }
     
+    
+    int solveTab( int start, int size, vector<int> &arr){
+        
+        vector<int> dp(size+2, 0);
+        
+        for( int i = size -1; i>= start; i-- ){
+        
+            int incl = arr[i] + dp[i+2];
+            int excl = dp[i+1];
+            dp[i] = max(incl, excl);
+            
+        }
+        
+        return dp[start];
+    }
     int rob(vector<int>& nums) {
         
         if( nums.size() == 1) return nums[0];
         int size = nums.size();
         
-        vector<int> dp1(size,-1);
-        vector<int> dp2(size,-1);
+//         Using Memo
+        // vector<int> dp1(size,-1);
+        // vector<int> dp2(size,-1);
+        // return max(solve(0,size-1,nums,dp1) , solve(1,size,nums,dp2));
         
-        return max(solve(0,size-1,nums,dp1) , solve(1,size,nums,dp2));
+//         Using Tabulation method
+        
+        return max(solveTab(0,size-1, nums), solveTab(1,size,nums));
     }
 };
