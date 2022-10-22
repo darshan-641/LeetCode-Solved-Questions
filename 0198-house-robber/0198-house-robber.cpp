0380586c50beefa985dba928d87de78ed079905a
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-22 12:22:29 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,7 +17,26 @@ public:
     }
     
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
-        return solve(nums.size(), nums);
+        // vector<int> dp(nums.size()+1,-1);
+        // return solve(nums.size(), nums);
+        
+        int next1 = 0;
+        int next2 = 0;
+        
+        int curr;
+        
+        for( int i= nums.size()-1; i>=0; i--){
+            
+            int incl = nums[i] + next2;
+            int excl = next1;
+            
+            curr = max(incl,excl);
+            
+            next2 = next1;
+            next1 = curr;
+            
+        }
+        
+        return curr;
     }
 };
