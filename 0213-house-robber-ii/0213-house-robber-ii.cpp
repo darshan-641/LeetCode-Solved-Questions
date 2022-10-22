9887028a42688074f55e7b87372c359e78db8847
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-22 14:02:08 +0530

    Time: 4 ms (42.43%), Space: 8 MB (19.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -16,59 +16,14 @@ public:
         
     }
     
-    
-    int solveTab( int start, int size, vector<int> &arr){
-        
-        vector<int> dp(size+2, 0);
-        
-        for( int i = size -1; i>= start; i-- ){
-        
-            int incl = arr[i] + dp[i+2];
-            int excl = dp[i+1];
-            dp[i] = max(incl, excl);
-            
-        }
-        
-        return dp[start];
-    }
-    
-    int solveSO( int start, int size, vector<int> &arr){
-        
-       int next1 = 0;
-        int next2 = 0;
-        int curr;
-        
-        for( int i = size -1; i>= start; i--){
-            
-            int incl = next2 + arr[i];
-            int excl = next1;
-            
-            curr = max( incl, excl);
-            
-            next2 = next1;
-            next1 = curr;
-            
-        }
-        
-        return curr;
-        
-    }
-    
     int rob(vector<int>& nums) {
         
         if( nums.size() == 1) return nums[0];
         int size = nums.size();
         
-//         Using Memo
-        // vector<int> dp1(size,-1);
-        // vector<int> dp2(size,-1);
-        // return max(solve(0,size-1,nums,dp1) , solve(1,size,nums,dp2));
+        vector<int> dp1(size,-1);
+        vector<int> dp2(size,-1);
         
-//         Using Tabulation method
-        // return max(solveTab(0,size-1, nums), solveTab(1,size,nums));
-        
-        
-//         Using SO
-        return max( solveSO(0,size-1, nums), solveSO(1,size, nums));
+        return max(solve(0,size-1,nums,dp1) , solve(1,size,nums,dp2));
     }
 };
