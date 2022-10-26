b184a32052e3d6104e0f8fa703ad9c737a688a09
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-26 14:11:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.65%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -33,10 +33,29 @@ public:
         return dp[n];
     }
     
+    int spaceOP( int n){
+        if(n==0) return 0;
+        if( n <= 2) return 1;
+        int prev3 = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 0;
+        for( int i =3 ; i<=n; i++){
+            curr = prev1 + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return curr;
+    }
+    
     int tribonacci(int n) {
       vector<int> dp(n+1, -1);
     // return solve( n,dp);
         
-        return tabulation(n);
+        // return tabulation(n);
+        
+        return spaceOP(n);
     }
 };
