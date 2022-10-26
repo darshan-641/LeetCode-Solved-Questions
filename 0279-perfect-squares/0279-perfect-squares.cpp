374be22e35aa1c6c5c7e16c61cc5d17d5365f9dc
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-27 00:09:06 +0530

    Time: 385 ms (50.62%), Space: 9 MB (69.15%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -18,8 +18,33 @@ public:
         return dp[n] = ans;
         
     }
+    
+    int Tab( int n){
+//         step 1: create dp array
+        vector<int> dp(n+1, 0);
+        
+//         step 2: decide the range
+        
+        for( int i =1; i<=n; i++){
+            
+             
+        int ans = INT_MAX;
+        
+        for( int k =1; k*k<=i; k++){
+            
+            ans = min(ans, 1+solve(i-(k*k), dp));
+            
+        }
+            dp[i] = ans;
+        }
+        
+//        return n;
+        return dp[n];
+    }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(n,dp);
+        
+        return Tab(n);
     }
 };
