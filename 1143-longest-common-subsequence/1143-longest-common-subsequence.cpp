43956232cc0399fb9e6393a192fc98cb59c4b308
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-29 19:38:31 +0530

    Time: 62 ms (24.23%), Space: 13 MB (46.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -19,12 +19,39 @@ public:
         
         return dp[i][j] = ans;
     }
+    
+    int solveTab(string &t1, string &t2, int s1, int s2){
+        
+//         step1: create DP array
+        vector<vector<int>> dp(s1+1, vector<int>(s2+1, 0));
+        
+        for( int i = s1-1; i>=0; i--){
+            
+            
+            for( int j =s2-1; j>=0; j--){
+               
+             int ans = 0;   
+            if( t1[i] == t2[j]){
+                ans = 1 + dp[i+1][j+1];
+            }
+            else{
+                ans = max(dp[i+1][j], dp[i][j+1]);
+            }
+
+            dp[i][j] = ans;
+
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
     int longestCommonSubsequence(string t1, string t2) {
         
-        vector<vector<int>> dp(t1.size()+1, vector<int>(t2.size()+1, -1));
+//         vector<vector<int>> dp(t1.size()+1, vector<int>(t2.size()+1, -1));
         
-        int ans = solve(t1,t2,0,0,dp);
-        
+//         int ans = solve(t1,t2,0,0,dp);
+        int ans = solveTab(t1,t2,t1.size(), t2.size());
         return ans;
     }
 };
