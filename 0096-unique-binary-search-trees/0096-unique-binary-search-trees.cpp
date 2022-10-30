27be2040100bff4bd012e9f50bf9a62704834223
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-30 14:31:34 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.75%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -15,9 +15,37 @@ public:
         
        return dp[n] = ans; 
     }
+    
+    int solveTab(int n){
+          if( n == 0 || n == 1) return 1;
+        if( n == 2) return 2;
+        
+        
+        vector<int> dp(n+1, 0);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for( int i =3; i<=n; i++ ){
+            
+        int ans = 0;
+        
+        for( int j=1; j<=i; j++){
+            
+            ans += dp[j-1] * dp[i-j];
+        }
+    
+        dp[i] = ans; 
+            
+        }
+        return dp[n];
+    }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(n,dp);
+        
+        return solveTab(n);
 
     }
 };
