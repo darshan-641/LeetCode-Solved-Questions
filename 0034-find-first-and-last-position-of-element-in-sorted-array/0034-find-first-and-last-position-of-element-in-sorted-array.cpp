c5a702c8a122415cb05a66c69bc6fce47e8e77a5
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-10-30 21:06:11 +0530

    Time: 19 ms (42.16%), Space: 13.7 MB (18.82%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int checkFirst(vector<int>&nums, int target){
+        
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = start + (end - start)/2;
+        
+        int index = -1;
+        
+        while( start <= end){
+            
+            if( nums[mid] == target){
+                end = mid - 1;
+                index = mid;
+            }
+            else if( nums[mid] < target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+            
+            mid = start + (end - start)/2;
+        }
+        return index;
+    }
+    int checkSecond(vector<int>& nums, int target){
+        
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = start + (end - start)/2;
+        
+        int index = -1;
+        
+        while( start <= end){
+            
+            if( nums[mid] == target){
+                start = mid + 1;
+                index = mid;
+            }
+            else if( nums[mid] < target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+            
+            mid = start + (end - start)/2;
+        }
+        return index;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+     
+        int first = checkFirst(nums,target);
+        if( first == -1) return {-1,-1};
+        
+        int second = checkSecond(nums,target);
+        if( second == -1) return {first,first};
+        
+        return {first, second};
+    }
+};
