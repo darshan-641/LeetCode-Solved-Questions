abb294df772d162acd0715bcc3dc90fdb0ca9b74
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-11-09 19:29:04 +0530

    Time: 44 ms (54.01%), Space: 12.3 MB (41.74%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int solve( string &s1, string &s2, int i , int j, vector<vector<int>> &dp){
+    
+    int solve( string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
         
-        if( i == s1.size() || j == s2.size()) return 0;
+        if( i == s1.length() && j == s2.length()) return 0;
+        
+        if( i == s1.length() && j < s2.length()) return s2.length() - j;
+        if( i < s1.length() && j == s2.length()) return s1.length() - i;
+        
+        
         if( dp[i][j] != -1) return dp[i][j];
-        int find = 0;
-        int notFound = 0;
+        
+        int find = INT_MAX;
+        int notFound = INT_MAX;
         
         if( s1[i] == s2[j]){
-            find = 1 + solve(s1,s2, i+1, j+1,dp);
+            find = solve( s1, s2, i+1, j+1,dp);
         }
         else{
-            notFound =  max( solve(s1, s2, i+1,j,dp), solve( s1,s2, i, j+1,dp));
+            notFound = 1 + min( solve(s1,s2, i+1, j,dp), solve(s1,s2,i, j+1,dp));
         }
         
-        return dp[i][j] = max( find, notFound);
+        return dp[i][j] = min(find, notFound);
     }
     int minDistance(string &s1, string &s2) {
         int n = s1.length();
         int m = s2.length();
         
-        
-        
-        vector<vector<int>> dp( n+1, vector<int>(m+1,-1));
-        int ans = solve(s1,s2,0,0,dp);
-        return (n+m) - 2* ans ;
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        return solve(s1, s2, 0,0,dp);
     }
 };
