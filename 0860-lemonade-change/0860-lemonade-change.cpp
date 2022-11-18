4860e6c41b87ad1f1ea8f0499a2c4e397972458e
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-11-18 15:09:42 +0530

    Time: 95 ms (99.94%), Space: 83.5 MB (9.06%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -3,30 +3,31 @@ public:
     bool lemonadeChange(vector<int>& bills) {
        
         // int cash = 0;
-        unordered_map<int,int> mp;
+        // unordered_map<int,int> mp;
+        int mp[3] = {};
         for( int i =0; i<bills.size(); i++){
             
             if( bills[i] == 5){
                 // cash += 5;
-                mp[5]++;
+                mp[0]++;
             }
             else if( bills[i] == 10){
                 // cash += 5;
-                mp[10]++;
-                if( mp[5] >=1) mp[5]--;
+                mp[1]++;
+                if( mp[0] >=1) mp[0]--;
                 else return false;
             }
             else if( bills[i] == 20){
                 
                 // cash += 5;
-                mp[20]++;
-                if( mp[10] >= 1 && mp[5]>=1) {
-                    mp[10]--;
-                    mp[5]--;
+                mp[2]++;
+                if( mp[1] >= 1 && mp[0]>=1) {
+                    mp[1]--;
+                    mp[0]--;
                     
                 }
-                else if( mp[5] >=3){
-                    mp[5] = mp[5] -3;
+                else if( mp[0] >=3){
+                    mp[0] = mp[0] -3;
                 }
                 else return false;
             }
