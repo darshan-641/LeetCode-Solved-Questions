4962797f7e1fd9c177e2e5414036985d853859b3
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-11-18 22:23:06 +0530

    Time: 3 ms (65.96%), Space: 6.1 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    vector<int> primes {2,3,5};
+    bool isUgly(int n) {
+        
+    
+            if( n<=0) return false;
+            
+            for( int p: primes){
+                
+                while( n%p == 0){
+                    n /= p;
+                }
+                
+            }
+        
+        return n==1;
+    }
+};
