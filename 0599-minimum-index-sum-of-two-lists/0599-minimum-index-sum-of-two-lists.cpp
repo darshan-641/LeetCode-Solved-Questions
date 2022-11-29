6eb0a47ef4f1c85897b200eeeeee6b17c4029b8d
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-11-29 12:05:38 +0530

    Time: 103 ms (89.37%), Space: 36.9 MB (57.31%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -2,37 +2,29 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         
-        unordered_map<string,pair<int,int>> mp;
+        
+        unordered_map<string,int> mp;
         
         for( int i =0; i<list1.size(); i++){
-            
-            mp[list1[i]].first++;
-            mp[list1[i]].second = i;
-            
+            mp[list1[i]] = i;
         }
         
         int mini = INT_MAX;
         vector<string> ans;
-        for( int i=0; i<list2.size(); i++){
-            mp[list2[i]].first++;
-            mp[list2[i]].second = i+  mp[list2[i]].second;
+        
+        for( int j =0; j<list2.size(); j++){
             
-            if( mp[list2[i]].first >1){
-                // cout<<"Entered";
-                if( mp[list2[i]].second < mini){
-                    ans.clear();
-                    ans.push_back(list2[i]);
-                    mini = mp[list2[i]].second;
-                }
+            if( mp.find(list2[j]) != mp.end()){
                 
-                else if( mp[list2[i]].second == mini){
-                    
-                    ans.push_back(list2[i]);
-                    
+                if( mp[list2[j]] + j < mini){
+                    ans.clear();
+                    mini = mp[list2[j]] + j;
+                    ans.push_back(list2[j]);
+                }
+                else if( mp[list2[j]] + j == mini){
+                    ans.push_back(list2[j]);
                 }
             }
-                
-            
         }
         
         return ans;
