4b533a5ed3da96db4368c1a0f99ee930a6584070
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-12-08 12:28:01 +0530

    Time: 413 ms (33.68%), Space: 48.2 MB (64.67%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    bool dfs( int node, vector<vector<int>> &graph, vector<int> &pathvis, vector<int> &vis, vector<int> &check){
+        
+        vis[node] = 1;
+        pathvis[node]  = 1;
+        
+        
+        for( auto i : graph[node]){
+            
+            if( !vis[i]){
+                if(dfs(i,graph,pathvis,vis,check)) return true;
+            }
+            else if( pathvis[i]) return true;
+        }
+        
+        
+        check[node] = 1;
+        pathvis[node] = 0;
+        return false;
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+          
+        
+        int n = graph.size();
+        unordered_map<int,list<int>> adj;
+        
+      
+     
+        vector<int> check(n,0);
+       
+        
+        vector<int> pathvis(n,0);
+        vector<int> vis(n,0);
+        for( int  i =0; i<n; i++){
+            
+            if( !vis[i]){
+                dfs(i,graph,pathvis,vis,check);
+            }
+        }
+        
+        vector<int> ans;
+        
+        for( int i =0; i<n; i++){
+            if( check[i]) ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
