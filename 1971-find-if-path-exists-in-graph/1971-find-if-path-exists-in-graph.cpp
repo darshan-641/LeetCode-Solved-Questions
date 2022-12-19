050050dbb126bf185c10f38f8f7f12322b460a85
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-12-19 10:02:30 +0530

    Time: 1703 ms (18.44%), Space: 278 MB (5.07%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -40,6 +40,22 @@ public:
         return false;
     } 
     
+    bool dfs( unordered_map<int, list<int>> &adj, unordered_map<int, bool> &vis, int node, int &dest){
+        
+        
+        vis[node] = 1;
+        
+        if( node == dest) return true;
+        
+        for( auto i : adj[node]){
+            
+            if( !vis[i]){
+                if( dfs(adj,vis,i,dest)) return true;
+            }
+        }
+        
+        return false;
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
        
         // if( source == destination) return true;
@@ -51,6 +67,6 @@ public:
         unordered_map<int, bool> visited;
         
     
-        return bfs( adj, visited, source, destination);
+        return dfs( adj, visited, source, destination);
     }
 };
