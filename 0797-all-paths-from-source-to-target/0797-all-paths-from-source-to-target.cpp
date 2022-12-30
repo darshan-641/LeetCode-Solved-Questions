2e943b577a468cac390a93593d4f4c5fc4c7bc49
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2022-12-30 20:39:48 +0530

    Time: 12 ms (91.70%), Space: 10.4 MB (99.87%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,78 +1,38 @@
 class Solution {
-public:
+    private:
     
-//     void dfs( unordered_map<int,list<int>> &adj, unordered_map<int, bool> &visited,  vector<int> &temp, vector<vector<int>> &ans, int node, int size){
-// //         base case
-        
-//         if(temp.size() == size){
-//             ans.push_back(temp);
-//             return ;
-//         }
-        
-//         visited[node] = true;
-//         temp.push_back(node);
-        
-//         for( auto i: adj[node]){
-            
-//             if( !visited[i]){
-                
-//                 dfs(adj,visited,temp,ans,i,size);
-//                 temp.pop_back();
-//             }
-//         }
-        
-// }
-        
-    vector<vector<int>> ans;
-    vector<int> path;
-    int target;
-    
-    void dfs( vector<vector<int>>& graph, int node = 0 ){
-        
-        path.push_back(node);
+    void dfs( int node, int &target, vector<vector<int>>& g, vector<int> &v, vector<vector<int>> &ans){
+
+    v.push_back(node);
         
         if( node == target){
-            ans.push_back(path);
+            ans.push_back(v);
+            return ;
         }
-        else{
+        
+        
+        for( auto it : g[node]){
             
-            for( auto i: graph[node]){
-                dfs(graph,i);
-            }
+            dfs( it, target,g, v,ans);
+             v.pop_back();
         }
         
-        path.pop_back();
+     
+        return ;
     }
-        
+public:
 
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
         
-        target = graph.size()-1;
         
-        dfs(graph);
+        
+        vector<int> v;
+        vector<vector<int>> ans;
+        int n = g.size()-1;
+        dfs( 0, n, g, v,ans);
         
         return ans;
-        
-// //         creating adjanency matrix
-        
-//         unordered_map<int,list<int>> adj;
-        
-//         for( int i =0; i<graph.size(); i++){
-            
-//             int u = graph[i][0];
-//             int v = graph[i][1];
-            
-//             adj[u].push_back(v);
-//         }
-        
-// //         create visited and ans vector 
-// //         DFS call
-        
-//         unordered_map<int, bool> visited;
-//         vector<vector<int>> ans;
-//         vector<int> temp;
-//         int size = graph.size()-1;
-//         dfs(adj,visited,temp,ans,0,size);
-//         return ans;
     }
 };
