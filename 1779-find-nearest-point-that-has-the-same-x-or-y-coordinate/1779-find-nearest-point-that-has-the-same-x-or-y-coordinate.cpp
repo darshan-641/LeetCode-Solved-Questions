74e4a9ab9be1186df04bf1da7480db5201459beb
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-01-14 20:01:32 +0530

    Time: 187 ms (68.94%), Space: 59.5 MB (36.96%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -6,6 +6,9 @@ public:
         
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         
+        pq.push({INT_MAX, -1});
+        
+        
         for( int i =0; i<p.size(); i++){
             
             int a = p[i][0];
@@ -15,12 +18,19 @@ public:
             int d2 = abs(y-b);
             
             if( d1 == 0 && d2 == 0) return i;
-            else if( d1 ==0 || d2 == 0) pq.push( {d1 + d2, i});
+            else if( d1 ==0 || d2 == 0) {
+                
+                if( pq.top().first > d1 + d2){
+                    
+                    pq.pop();
+                    pq.push({d1+d2, i});
+                }
+            }
         
         }
         
         
-        if( pq.empty()) return -1;
+        if( pq.top().second == -1) return -1;
         else return pq.top().second;
         
         
