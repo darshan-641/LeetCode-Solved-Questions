feeba0838c6ea4a14b56d397944440c0c728481a
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-01-23 19:14:26 +0530

    Time: 508 ms (6.96%), Space: 69 MB (29.65%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         
-        vector<int> indeg(n+1,0), outdeg(n+1,0);
+        // vector<int> indeg(n+1,0), outdeg(n+1,0);
         
+        int* indeg = new int[n+1];
+        int* outdeg = new int[n+1];
         
+        for( int i =0; i<=n; i++){
+            indeg[i] = 0;
+            outdeg[i] = 0;
+        }
         for( auto a : trust){
             indeg[a[1]]++;
             outdeg[a[0]]++;
