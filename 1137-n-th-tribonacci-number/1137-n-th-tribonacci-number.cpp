6b51788a1aae8107c389f704793e77e055c2f29c
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-01-30 16:23:30 +0530

    Time: 3 ms (38.83%), Space: 6.1 MB (22.73%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,61 +1,15 @@
 class Solution {
-public:
-    
-    int solve( int n, vector<int> &dp){
+    int f(int n, vector<int> &dp){
         
-        if( n<=0) return 0;
-        if( n== 1 || n ==2) return 1;
-        
+        if( n <= 0) return 0;
+        if( n  == 1) return 1;
+        if( n == 2) return 1;
         if( dp[n] != -1) return dp[n];
-        
-        int a = solve(n-1,dp) + solve(n-2,dp) + solve(n-3,dp);
-        
-        return dp[n]=a;
-        
+        return dp[n] = f(n-1,dp) + f(n-2,dp) + f(n-3,dp);
     }
-    
-    int tabulation( int n){
-        
-//         creating dp array
-        if( n == 0) return 0;
-        
-        if( n <=2) return 1;
-        vector<int> dp(n+1,0);
-        
-        dp[1] = 1;
-        dp[2]  =1;
-        
-        for( int i =3; i<=n; i++){
-            
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        }
-        
-        return dp[n];
-    }
-    
-    int spaceOP( int n){
-        if(n==0) return 0;
-        if( n <= 2) return 1;
-        int prev3 = 0;
-        int prev1 = 1;
-        int prev2 = 1;
-        int curr = 0;
-        for( int i =3 ; i<=n; i++){
-            curr = prev1 + prev2 + prev3;
-            prev3 = prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        
-        return curr;
-    }
-    
+public:
     int tribonacci(int n) {
-      vector<int> dp(n+1, -1);
-    // return solve( n,dp);
-        
-        // return tabulation(n);
-        
-        return spaceOP(n);
+        vector<int> dp(n+1, -1);
+        return f(n,dp);
     }
 };
