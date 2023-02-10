eec663a9ed008376a43ca91d3ac22cea1cff3674
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-02-10 13:13:19 +0530

    Time: 8 ms (73.41%), Space: 9.4 MB (14.90%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        
+        long long ans = 0;
+        
+        priority_queue<int> pq;
+        
+        for( auto i: gifts){
+            pq.push(i);
+            
+        }
+        
+        while( k--){
+            
+         pq.push(sqrt(pq.top()));
+            pq.pop();
+            
+          
+        }
+        while( !pq.empty()){
+            
+            ans += pq.top();
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
