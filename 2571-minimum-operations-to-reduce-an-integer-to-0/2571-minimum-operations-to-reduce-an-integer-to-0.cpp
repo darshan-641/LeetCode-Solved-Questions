625e376b19cfd64c933b0c9c4954465e9cfa1584
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-02-19 23:18:14 +0530

    Time: 3 ms (57.14%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        
+       int x = 1, y = 1;
+      if(n <= 0) return 0;  
+      if(n == 1) return 1;
+      while(x * 2 < n) x *= 2;
+      y = x * 2 - n;
+      x = n - x;
+      int low = minOperations(x);
+      int high = minOperations(y);
+      return min(low, high) + 1;
+    }
+};
